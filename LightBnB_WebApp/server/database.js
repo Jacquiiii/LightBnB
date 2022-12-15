f2b15bb2fe4a0d59a82b203b3b3563538ad1fb50
Jacquiiii <jacqui.koroll@telus.com>
2022-12-15 14:14:55 -0800

    refactor getAllProperties in database.js to pull from database

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -22,13 +22,8 @@ const pool = new Pool({
 const getUserWithEmail = (email) => {
   return pool
     .query(`SELECT * FROM users WHERE email = LOWER($1);`, [email.toLowerCase()])
-    .then((result) => {
-      if (!result.rows[0]) {
-        return Promise.reject('failed to login');
-      }
-      return result.rows[0];
-    })
-    .catch((err) => console.log(err));
+    .then((result) => result.rows[0])
+    .catch((err) => console.log(err.message));
 };
 
 exports.getUserWithEmail = getUserWithEmail;
@@ -83,9 +78,27 @@ exports.addUser = addUser;
  * @param {string} guest_id The id of the user.
  * @return {Promise<[{}]>} A promise to the reservations.
  */
-const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
-}
+
+
+ const getAllReservations = (guest_id, limit = 10) => {
+  const queryString = `
+  SELECT reservations.*, properties.*, avg(rating) as average_rating
+  FROM reservations
+  JOIN properties ON reservations.property_id = properties.id
+  JOIN property_reviews ON properties.id = property_reviews.property_id
+  WHERE reservations.guest_id = $1
+  GROUP BY properties.id, reservations.id
+  ORDER BY reservations.start_date
+  LIMIT $2;
+  `;
+  const values = [guest_id, limit];
+
+  return pool
+    .query(queryString, values)
+    .then((result) => result.rows)
+    .catch((err) => console.log(err.message));
+};
+
 exports.getAllReservations = getAllReservations;
 
 
